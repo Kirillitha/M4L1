e18eab154cfcb8c33fbd2e00b0bc2f284147bd35
Kirillitha <kirill.atyashev@yandex.ru>
2026-02-21 15:55:32 +0300

    logic.py

--- logic.py
+++ logic.py
@@ -69,28 +69,51 @@ class DatabaseManager:
             conn.execute('''UPDATE prizes SET used = 1 WHERE prize_id = ?''', (prize_id,))
             conn.commit()
 
-def get_users(self):
+    def get_users(self):
         conn = sqlite3.connect(self.database)
         with conn:
-            cur = conn.cursor("SELECT * FROM users")
-            cur.execute()
+            cur = conn.cursor()
+            cur.execute("SELECT * FROM users")
         return [x[0] for x in cur.fetchall()]
 
         
-def get_prize_img(self, prize_id):
+    def get_prize_img(self, prize_id):
         conn = sqlite3.connect(self.database)
         with conn:
-            cur = conn.cursor("SELECT image FROM prizes WHERE prize_id = ?", (prize_id,))
-            cur.execute()
+            cur = conn.cursor()
+            cur.execute("SELECT image FROM prizes WHERE prize_id = ?", (prize_id,))
         return cur.fetchall()[0][0]
 
 
-def get_random_prize(self):
+    def get_random_prize(self):
         conn = sqlite3.connect(self.database)
         with conn:
-            cur = conn.cursor("SELECT * FROM prizes WHERE used = 0")
-            cur.execute()
+            cur = conn.cursor()
+            cur.execute("SELECT * FROM prizes WHERE used = 0")
         return cur.fetchall()[0]
+
+    def get_winners_count(self, prize_id):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute('SELECT COUNT(*) FROM winners WHERE prize-id = ?', (prize_id, ))
+        return cur.fetchall()[0][0]
+   
+   
+    
+    def get_rating(self):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute('''
+SELECT user.user_name, COUNT(winners.prize_id) AS count_prizes
+FROM winners                        
+INNER JOIN users ON user.user_name = winners.user_name
+GROUP BY winners.user_id                        
+ORDER BY count_prizes DESK
+LIMIT 10                        
+''')
+        return cur.fetchall()
 
 def hide_img(img_name):
     image = cv2.imread(f'img/{img_name}')
